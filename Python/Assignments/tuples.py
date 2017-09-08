7ae4d47fa45cf96f18bcd8e5dbff7d16742bf506
Grace <gsongk@gmail.com>
2017-09-08 16:16:49 -0600

    Addded tuples

---
+++ Python/Assignments/tuples.py
@@ -0,0 +1,14 @@
+# Tuples
+
+# function input
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+my_dict.items()
+
+[(v,k) for k, v in my_dict.iteritems()]
+
+print my_dict

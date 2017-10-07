b2226165a985feec540298763fcf9ae50f429c2d
Grace <gsongk@gmail.com>
2017-10-06 20:58:07 -0600

    added optional assignments in python week 1

--- Python/Week1/fooBar.py
+++ Python/Week1/fooBar.py
@@ -2,13 +2,15 @@
 
 def fooBar():
     print "Foo is for prime, Bar is for Square"
+  
     for x in range (100, 100000):
         # if statement - prime
-        # print "Foo"
         if (x % 2) != 0 and (x % 3) != 0 and (x % 5) != 0 and (x % 7) != 0 and (x % 9) != 0 and (x % 11) != 0:
             print "prime number", x ,"Foo" 
         # perfect squares
-        # if x/x == 1:
-        #     print "Bar"
+        square = x ** (1.0/2)
+        if (square).is_integer():
+            True
+            print x, "Squared: ", square, " Bar"
 
 fooBar()

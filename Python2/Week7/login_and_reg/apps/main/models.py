00016a78d9a059ff2355ed4f0d38c7a66c59dbf2
Grace <gsongk@gmail.com>
2017-11-25 19:37:44 -0700

    in prep of review

--- Python2/Week7/login_and_reg/apps/main/models.py
+++ Python2/Week7/login_and_reg/apps/main/models.py
@@ -1,6 +1,72 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import re
+import bcrypt
 from django.db import models
 
-# Create your models here.
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+NAME_REGEX = re.compile(r'^[A-Za-z]\w+$')
+
+
+class UserManager(models.Manager):
+    def validate_login(self, post_data):
+        errors = []
+        # check DB for post_data['email']
+        if len(self.filter(email=post_data['email'])) > 0:
+            # check this user's password
+            user = self.filter(email=post_data['email'])[0]
+            if not bcrypt.checkpw(post_data['password'].encode(), user.password.encode()):
+                errors.append('email/password incorrect')
+        else:
+            errors.append('email/password incorrect')
+
+        if errors:
+            return errors
+        return user
+
+    def validate_registration(self, post_data):
+        errors = []
+        # check length of name fields
+        if len(post_data['first_name']) < 2 or len(post_data['last_name']) < 2:
+            errors.append("name fields must be at least 3 characters")
+        # check length of name password
+        if len(post_data['password']) < 8:
+            errors.append("password must be at least 8 characters")
+        # check name fields for letter characters
+        if not re.match(NAME_REGEX, post_data['first_name']) or not re.match(NAME_REGEX, post_data['last_name']):
+            errors.append('name fields must be letter characters only')
+        # check emailness of email
+        if not re.match(EMAIL_REGEX, post_data['email']):
+            errors.append("invalid email")
+        # check uniqueness of email
+        if len(User.objects.filter(email=post_data['email'])) > 0:
+            errors.append("email already in use")
+        # check password == password_confirm
+        if post_data['password'] != post_data['password_confirm']:
+            errors.append("passwords do not match")
+
+        if not errors:
+            # make our new user
+            # hash password
+            hashed = bcrypt.hashpw(
+                (post_data['password'].encode()), bcrypt.gensalt(5))
+
+            new_user = self.create(
+                first_name=post_data['first_name'],
+                last_name=post_data['last_name'],
+                email=post_data['email'],
+                password=hashed
+            )
+            return new_user
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- Python2/Week7/login_and_reg/apps/main/views.py
+++ Python2/Week7/login_and_reg/apps/main/views.py
@@ -1,6 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from .models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'login/index.html')
+
+
+def register(request):
+    result = User.objects.validate_registration(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully registered!")
+    return redirect('/success')
+
+
+def login(request):
+    result = User.objects.validate_login(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully logged in!")
+    return redirect('/success')
+
+
+def success(request):
+    try:
+        request.session['user_id']
+    except KeyError:
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, 'login/success.html', context)

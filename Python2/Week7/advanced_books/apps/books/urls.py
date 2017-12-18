18224855a7e97edd4ab18f17b701980ecdd3413c
Grace <gsongk@gmail.com>
2017-12-17 19:42:11 -0700

    deleted files

--- Python2/Week7/advanced_books/apps/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^login$', views.login),
-    url(r'^register$', views.register),
-    url(r'^success$', views.success),
-]

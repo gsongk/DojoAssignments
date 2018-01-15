07c1d3892a114cd7c64740aecacd844699d98339
Grace <gsongk@gmail.com>
2018-01-15 13:30:51 -0700

    updated basic13

--- csharp/basic13/Program.cs
+++ csharp/basic13/Program.cs
@@ -4,9 +4,36 @@ namespace basic13
 {
     class Program
     {
+        //Prints values 1 to 255
+        public static void Print1to255(){
+            for (int x = 1; x<=255; x++){
+                Console.WriteLine(x);
+            }
+        }
+        //Print Odd Values
+        public static void PrintOdds(){
+            for (int x=1; x<=255; x++){
+                if (x%2==1){
+                    Console.WriteLine(x);
+                }
+            }
+        }
+        public static void PrintSum(){
+            int sum = 0;
+            for(int x=1; x<=255; x++){
+                sum += x;
+                Console.WriteLine($"New Number: {x} Sum: {sum}");
+            }
+        }
+        public static void Array(){
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Print1to255();
+            PrintOdds();
+            PrintSum();
+            Array();
         }
     }
 }

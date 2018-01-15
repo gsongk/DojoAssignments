bec09f9b9da20d7231a0d8e23737ef628716010e
Grace <gsongk@gmail.com>
2018-01-14 21:08:21 -0700

    added new files

--- csharp/boxing/Program.cs
+++ csharp/boxing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace boxing
 {
@@ -6,7 +8,23 @@ namespace boxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<object> mixedList = new List<object>();
+            mixedList.Add(7);
+            mixedList.Add(28);
+            mixedList.Add(-1);
+            mixedList.Add(true);
+            mixedList.Add("chair");
+
+            int sum = 0;
+            foreach (var item in mixedList){
+                Console.WriteLine(item);
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+            }
+            Console.WriteLine("The sum of all the integers in the list is {0}", sum);
+
         }
     }
 }

3b9c674b2b741087688eb466632833c1f0afed0b
Grace <gsongk@gmail.com>
2018-01-16 19:18:31 -0700

    Updated

--- csharp/puzzles/Program.cs
+++ csharp/puzzles/Program.cs
@@ -4,6 +4,9 @@ namespace puzzles
 {
     class Program
     {
+        //Random Array
+        //Coin Flip
+        //Names
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");

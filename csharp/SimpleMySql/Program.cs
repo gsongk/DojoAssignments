299f8ed0bbe2d7abb7aa52f1642903634fc48c74
Grace <gsongk@gmail.com>
2018-01-29 17:21:38 -0700

    updated

---
+++ csharp/SimpleMySql/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DbConnection;
+
+namespace SimpleMySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

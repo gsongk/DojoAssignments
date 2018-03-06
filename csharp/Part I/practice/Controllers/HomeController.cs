fe7f7ff503a26316a7599b4d8c8418c989fd85ff
Grace <gsongk@gmail.com>
2018-03-06 16:52:30 -0700

    Updated practice

--- csharp/Part I/practice/Controllers/HomeController.cs
+++ csharp/Part I/practice/Controllers/HomeController.cs
@@ -10,28 +10,11 @@ namespace practice.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
